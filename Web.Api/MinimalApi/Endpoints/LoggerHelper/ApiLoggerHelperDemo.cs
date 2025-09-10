1556e76638f3a5868af98baa1eacb69498bbf483
a.chiodo <alexbypa@gmail.com>
2025-09-10 18:42:11 +0200

    Update SelfLog to check Error on boot

--- Web.Api/MinimalApi/Endpoints/LoggerHelper/ApiLoggerHelperDemo.cs
+++ Web.Api/MinimalApi/Endpoints/LoggerHelper/ApiLoggerHelperDemo.cs
@@ -7,6 +7,15 @@ public class ApiLoggerHelperDemo : IEndpointDefinition {
         app.MapGet("Logger/LogDemo", () => {
             Console.WriteLine("LoggerHelper Demo");
             loggerExtension<RequestHelper>.TraceSync(new RequestHelper() { IdTransaction = Guid.NewGuid().ToString() }, Serilog.Events.LogEventLevel.Information, null, "Test");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("CHECK SINKS: VERIFY SINKS LOADED:::::::");
+            Console.WriteLine($"CURRENTERROR: {GlobalLogger.CurrentError}");
+            Console.WriteLine($"TOT ERRORS: {string.Join(",", GlobalLogger.Errors.Select(a => a.SinkName + ' ' + a.ErrorMessage).ToArray())}");
+            GlobalLogger.SinksLoaded.ForEach(sink => 
+                Console.WriteLine($"Sink loaded: {sink.SinkName} with Level : {string.Join(",", sink.Levels.ToArray())}")
+            );
+            Console.WriteLine("CHECK SINKS: STOP VERIFY:::::::");
+            Console.ResetColor();
             return Results.Ok("LoggerHelper Demo");
         })
         .WithSummary("Simple Request")
