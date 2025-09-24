5ecee8ffae93e253cb7ce25ebdc251ebb24b8371
Alex <alexbypa@gmail.com>
2025-09-24 12:46:20 +0200

    Add topResultsOnQuery and fixed readme

--- Web.Api/SqlQueries/RagAnswerQuery/getLogs.sql
+++ Web.Api/SqlQueries/RagAnswerQuery/getLogs.sql
@@ -1,4 +1,4 @@
 select "Id", "ApplicationName" "App", "TimeStamp" "Ts", "LogEvent" "Message", "IdTransaction" "TraceId" from "LogEntry"  
-where "TimeStamp" > {now}
+where "TimeStamp" > @now
 order by "Id" desc
-limit {n}
+limit @n
