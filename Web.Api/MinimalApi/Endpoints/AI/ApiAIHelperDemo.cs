ff29c1b549395ac0650afe2186d615e999e9ecc7
a.chiodo <alexbypa@gmail.com>
2025-09-25 14:06:52 +0200

    Code Review SOLID #2

--- Web.Api/MinimalApi/Endpoints/AI/ApiAIHelperDemo.cs
+++ Web.Api/MinimalApi/Endpoints/AI/ApiAIHelperDemo.cs
@@ -3,8 +3,9 @@ using CSharpEssentials.LoggerHelper.AI.Domain;
 
 namespace Web.Api.MinimalApi.Endpoints.AI;
 public class ApiAIHelperDemo : IEndpointDefinition {
+
     public void DefineEndpoints(WebApplication app) {
-        app.MapPost("/IAI/run", async (IActionOrchestrator orc, MacroContext ctx, CancellationToken ct) =>
+        app.MapPost("/IAI/run", async (IActionOrchestrator orc, IMacroContext ctx, CancellationToken ct) =>
             Results.Ok(await orc.RunAsync(ctx, ct)))
                 .WithTags("AI")
                 .WithSummary("Demo")
