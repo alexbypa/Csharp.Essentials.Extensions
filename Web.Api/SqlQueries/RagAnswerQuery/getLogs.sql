36c4539f0a1bafb69dfee5ce68bc6f6bbf4986e1
Alex <alexbypa@gmail.com>
2025-09-25 21:40:12 +0200

    Push code review

--- Web.Api/SqlQueries/RagAnswerQuery/getLogs.sql
+++ Web.Api/SqlQueries/RagAnswerQuery/getLogs.sql
@@ -1,4 +1,4 @@
 select "Id", "ApplicationName" "App", "TimeStamp" "Ts", "LogEvent" "Message", "IdTransaction" "TraceId" from "LogEntry"  
-where "TimeStamp" > @now
+where "TimeStamp" > @dtStart and "Message" like '%401%'
 order by "Id" desc
 limit @n
