803e8f3deca53cbf6eee3286411f85bed940a10e
a.chiodo <alexbypa@gmail.com>
2025-09-26 10:14:11 +0200

    Code Review
    Refactor service registrations and clean up code
    
    - Added CORS support and improved service registration in `Program.cs`.
    - Streamlined `ILogMacroAction` registrations in `AiServiceExtensions.cs`.
    - Removed unnecessary using directives in `CorrelateTraceAction.cs`.
    - Cleaned up commented-out code in `SummarizeIncidentAction.cs`.
    - Simplified database connection logic in `SqlAiPersistenceExtensions.cs`.

--- Web.Api/Program.cs
+++ Web.Api/Program.cs
@@ -46,66 +46,6 @@ builder.Services.AddCors(opt => {
     builder.Services.AddCSharpEssentialsLoggerAI(builder.Configuration,
             SqlAiPersistenceFactory.AddSqlAiPersistence(builder.Configuration) // <-- Passa l'Action<IServiceCollection>
 );
-
-builder.Services
-    .AddOptions<LoggerAIOptions>()
-    .Bind(builder.Configuration.GetSection("LoggerAIOptions"))
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-
-// --- SEZIONE DATABASE ---
-// Qui rendiamo l'applicazione flessibile, in grado di passare da un database
-// all'altro cambiando solo una riga nel file di configurazione appsettings.json.
-// -> Legge il valore "DatabaseProvider" dalla configurazione.
-if (builder.Configuration.GetValue<string>("DatabaseProvider")!.Contains("postgresql", StringComparison.InvariantCultureIgnoreCase)) {
-    builder.Services.AddScoped(_ => new NpgsqlConnection(builder.Configuration.GetConnectionString("Default")));
-    builder.Services.AddScoped<IWrapperDbConnection>(_ => new FactoryPostgreSqlConnection(builder.Configuration.GetConnectionString("Default")!));
-} else {
-    builder.Services.AddScoped(_ => new SqlConnection(builder.Configuration.GetConnectionString("Default")));
-    builder.Services.AddScoped<IWrapperDbConnection>(_ => new FactorySQlConnection(builder.Configuration.GetConnectionString("Default")!));
-}
-
-// --- SEZIONE REPOSITORY (Livello Accesso Dati) ---
-// I repository sono classi che contengono la logica per interrogare il database.
-// Dipendono da 'IWrapperDbConnection' che abbiamo registrato sopra.
-// -> Quando una classe chiede 'ILogRepository', gli viene data un'istanza di 'SqlLogRepository'.
-
-//builder.Services.AddScoped<ILogRepository, SqlLogRepository>();
-//builder.Services.AddScoped<ITraceRepository<TraceRecord>, SqlTraceRepository>();
-//builder.Services.AddScoped<IMetricRepository, SqlMetricRepository>();
-
-// -> Registra il servizio per creare gli embedding (vettori numerici dal testo).
-builder.Services.AddScoped<IEmbeddingService, NaiveEmbeddingService>();
-
-// -> Registra il nostro "costruttore di dati" da file. È 'Transient' perché è leggero,
-//    senza stato, e vogliamo un'istanza nuova ogni volta che viene usato.
-builder.Services.AddTransient<FileLogIndexer>(); // se vuoi usarlo per popolare il vettore store da file
-
-builder.Services.AddTransient<IFileLoader, FileLoader>();
-
-var serviceProvider = builder.Services.BuildServiceProvider();
-var fileLoader = serviceProvider.GetRequiredService<IFileLoader>();
-var sqlModels = fileLoader.getModelSQLLMModels();
-builder.Services.AddSingleton(sqlModels);
-
-builder.Services.AddScoped<ILogVectorStore, SqlLogVectorStore>();
-//builder.Services.AddScoped<ILogVectorStore, InMemoryLogVectorStore>();
-
-//builder.Services.AddHostedService<VectorStoreInitializationService>();//TODO:
-
-// -> Registriamo più implementazioni per la stessa interfaccia 'ILogMacroAction'.
-//    Questo permette all'orchestratore di riceverle tutte in una lista e decidere quale usare.
-builder.Services.AddScoped<ILogMacroAction, SummarizeIncidentAction>();
-builder.Services.AddScoped<ILogMacroAction, CorrelateTraceAction>();
-builder.Services.AddScoped<ILogMacroAction, DetectAnomalyAction>();
-builder.Services.AddScoped<ILogMacroAction, RagAnswerQueryAction>();
-
-// -> Registra l'orchestratore, la classe che gestisce e coordina tutte le azioni.
-builder.Services.AddScoped<IActionOrchestrator, ActionOrchestrator>();
-
-builder.Services.AddScoped<ILlmChat, OpenAiLlmChat>(); // oppure
-
 #endregion
 
 
