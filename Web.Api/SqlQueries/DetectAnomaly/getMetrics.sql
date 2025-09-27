754c7aed8c9edb43dd98b885fe112eabeef94346
Alex <alexbypa@gmail.com>
2025-09-27 11:30:16 +0200

    Detect Anomaly code review

--- Web.Api/SqlQueries/DetectAnomaly/getMetrics.sql
+++ Web.Api/SqlQueries/DetectAnomaly/getMetrics.sql
@@ -1,4 +1,8 @@
-select M.*, T."TagsJson" "TraceJson" from "MetricEntry" M 
+select
+	M."TraceId", 
+	M."Value", 
+	T."TagsJson" "TraceJson" from "MetricEntry" M 
 	inner join "TraceEntry" T ON M."TraceId" = T."TraceId"
 	where M."TraceId" IS NOT NULL and M."Name" = 'db.client.commands.duration' 
-		order by M."Timestamp" desc limit 100
+		and "Timestamp" between '2025-09-20T08:00:00' and '2025-09-28T11:00:00'
+		order by M."Value" desc limit 10
--- Web.Api/SqlQueries/RagAnswerQuery/getMetrics.sql
+++ Web.Api/SqlQueries/RagAnswerQuery/getMetrics.sql
@@ -1,1 +1,6 @@
-select Id, ApplicationName App, TimeStamp Ts, LogEvent Message, IdTransaction TraceId from [MetricEntry]  order by id desc
+-- select Id, ApplicationName App, TimeStamp Ts, LogEvent Message, IdTransaction TraceId from [MetricEntry]  order by id desc
+
+SELECT "Timestamp1" "Ts", "Id", "TraceId", 'Demo' "App", "TagsJson" "Message", "Value" "duration" FROM "MetricEntry" where "Timestamp" >= '2025-09-27T07:00:00' and "Name" = 'http.server.request.duration' order by "Value" desc limit 20
+
+-- Which traceId had the highest duration ?
+-- You are a telemetry expert and can spot anomalies in HTTP calls
